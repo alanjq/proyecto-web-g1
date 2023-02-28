57759df3a60a269afb7c04a5e1c0244cdeaa74ca
Erik Jimenez Velasco <jive931221@gmail.com>
2023-02-27 18:51:03 -0600

    avance de proyecto

--- src/App.js
+++ src/App.js
@@ -4,23 +4,9 @@ import HomeSection from './sections/home/Home';
 
 function App() {
   return (
-    <main className='flex flex-col'>
-      <HomeSection />
-      <AboutSection/>
-
-      {/* tres columnas */}
-      <div className='flex-row flex mb-4'>
-        <p className='bg-blue-100 flex-auto'>Columna</p>
-        <p className='bg-blue-200 flex-auto'>Columna</p>
-        <p className='bg-blue-300 flex-auto'>Columna</p>
-      </div>
-
-      {/* dos */}
-      <div className='flex-row flex'>
-        <p className='bg-red-100 flex-auto'>Columna A</p>
-        <p className='bg-red-200 flex-auto'>Columna B</p>
-      </div>
-    </main>
+    <section className='container mx-auto'>
+      <AboutSection />
+    </section>
   );
 }
 
