b54a6f02b465e97951a56ed5468858f28a2be595
Alan Jimenez <alanjq@outlook.com>
2023-02-27 21:58:34 -0500

    Create Contact section

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import HomeSection from './sections/home/Home';
 import ServicesSection from './sections/services/ServicesSection';
 import WorkSection from './sections/work/WorkSection';
 import AboutSection from './sections/about/AboutSection';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import ContactSection from './sections/contact/ContactSection';
 
 function App() {
   return (
-    <main className='flex flex-col'>
-      <HomeSection />
+    <>
+      <Navbar />
 
-      <AboutSection />
+      <main className='flex flex-col'>
+        <HomeSection />
+        <AboutSection />
+        <ServicesSection />
+        <WorkSection />
+        <ContactSection />
+      </main>
 
-      <ServicesSection />
-
-      <WorkSection />
-    </main>
-
+      <Footer />
+    </>
   );
 }
 
---
+++ src/sections/contact/ContactSection.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ContactSection() {
+    return (
+        <div>
+            Contact section
+        </div>
+    )
+}
+export default ContactSection
