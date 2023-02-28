16b3b53121550ff8588cc66bff184d1c01ce3209
Alan Jimenez <alanjq@outlook.com>
2023-02-27 21:02:09 -0500

    Add all sections

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import HomeSection from './sections/home/Home';
 import ServicesSection from './sections/services/ServicesSection';
 import WorkSection from './sections/work/WorkSection';
+import AboutSection from './sections/about/AboutSection';
 
 function App() {
   return (
     <main className='flex flex-col'>
       <HomeSection />
+
+      <AboutSection />
 
       <ServicesSection />
 
