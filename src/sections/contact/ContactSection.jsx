3a1892c0fe04b519387f41e13e1e1686acc7844f
Alan Jimenez <alanjq@outlook.com>
2023-02-27 22:35:49 -0500

    Heading component

--- src/sections/contact/ContactSection.jsx
+++ src/sections/contact/ContactSection.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Container from '../../components/container/Container'
+import Heading from '../../components/heading/Heading';
+import ContactForm from './ContactForm'
 
 function ContactSection() {
     return (
-        <div>
-            Contact section
-        </div>
+        <Container>
+            <Heading sub="Get in touch">Let's talk</Heading>
+            
+            <ContactForm />
+        </Container>
     )
 }
 export default ContactSection
