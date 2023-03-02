c10376a94ec6067ff2e4dbb5378b588060d7b175
Erik Jimenez Velasco <jive931221@gmail.com>
2023-03-01 21:19:55 -0600

    Proyecto final

--- src/sections/about/AboutSection.jsx
+++ src/sections/about/AboutSection.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
 import aboutImage from '../../img/about-image.jpg'
 import Container from '../../components/container/Container';
+import Counter from './Counter';
+
 
 function AboutSection() {
+
+
     return (
         <Container>
             <div className='container md:mx-auto'>
@@ -19,27 +22,10 @@ function AboutSection() {
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                             </p>
                         </div>
-                        <div className='flex'>
-                            <div className='flex-1 text-[#ff9800] text-4xl font-bold'>
-                                15
-                            </div>
-                            <div className='flex-1 text-[#ff9800] text-4xl font-bold'>
-                                1200
-                            </div>
-                            <div className='flex-1 text-[#ff9800] text-4xl font-bold'>
-                                1500
-                            </div>
-                        </div>
-                        <div className='flex'>
-                            <div className='flex-1 text-sm font-medium'>
-                                Years Of Experience
-                            </div>
-                            <div className='flex-1  text-sm font-medium'>
-                                Satisfied Client
-                            </div>
-                            <div className='flex-1 text-sm font-medium'>
-                                Happy Customers
-                            </div>
+                        <div className='flex gap-8'>
+                            <Counter number={15} text="year of experience" />
+                            <Counter number={1200} text="Satisfied Client" />
+                            <Counter number={1500} text="Happy Customers" />
                         </div>
                         <div className=' flex '>
                             <div className='flex-none rounded-full border-2 my-6 border-black  hover:bg-[#ff9800] transition duration-500 '>
@@ -47,11 +33,11 @@ function AboutSection() {
                             </div>
                         </div>
                     </div>
-                    <div className='max-w-sm flex-1'>
-                        <img src={aboutImage} alt="Imagen de about" />
+                    <div className='flex-1'>
+                        <img className='max-w-lg ' src={aboutImage} alt="Imagen de about" />
                     </div>
                 </div>
             </div>
-        </Container>)
+        </Container >)
 }
 export default AboutSection
