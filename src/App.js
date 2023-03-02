5e86b1c90898ebab2a2fa0ddc0a86a69c6131ef1
Alan Jimenez <alanjq@outlook.com>
2023-03-01 23:09:16 -0500

    Secciones agregadas

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
-// import Box from './sections/services/box';
-// import Container from './components/container/Container';
 import HomeSection from './sections/home/Home';
 import ServicesSection from './sections/services/ServicesSection';
-// import WorkSection from './sections/work/WorkSection';
+import WorkSection from './sections/work/WorkSection';
 import AboutSection from './sections/about/AboutSection';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import ContactSection from './sections/contact/ContactSection';
+import TestimonialsSection from './sections/testimonials/TestimonialsSection';
 
 function App() {
   return (
-    <main className='flex flex-col'>
-      <HomeSection />
-      <AboutSection />
-      <ServicesSection />
-    </main>
-    
+    <>
+      <Navbar />
+
+      <main className='flex flex-col'>
+        <HomeSection />
+        <AboutSection />
+        <ServicesSection />
+        <WorkSection />
+        <ContactSection />
+        <TestimonialsSection />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
 
 export default App;
